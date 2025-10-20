5a953e6a29c90cfddda365a2e9bc4d162b76e19f
benji9812 <benjiw98@gmail.com>
2025-10-20 13:08:06 +0200

    Added Transaction.cs and BudgetManager.cs

---
+++ Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perosnal_Budget_Tracker
+{
+    public class Transaction // Class to represent a financial transaction
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Date { get; set; }
+
+        public void ShowInfo()  // Method to display transaction info with color coding
+        {
+            if (Amount < 0) // Red for expenses
+                Console.ForegroundColor = ConsoleColor.Red;
+            else // Green for income
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine($"{Date}: {Description} | {Amount} kr | {Category}");
+            Console.ResetColor();
+        }
+    }
+}
